1b05d1d6576ea6d6732bcf272051da7fb9353894
James-wasonga <jameswasonga22@gmail.com>
2023-12-18 08:33:06 +0300

    updates

---
+++ source/decisions.js
@@ -0,0 +1,42 @@
+/*
+let i = 5;
+
+if(i==4){
+    console.log('It is equal to i');
+}else {
+    console.log('It is not equal');
+}
+*/
+
+//switch statement
+/*
+let grade = 'A';
+
+switch(grade){
+
+case 'A':
+    console.log('Excellence performance');
+    break;
+case 'B':
+    console.log('Better Performance');
+    break;
+case 'C':
+    console.log('Better performance');
+    break;
+case 'D':
+    console.log('poor performance');
+    break;
+case 'E':
+    console.log('Failed');
+    break;
+default:
+    console.log('No result');               
+}
+*/
+
+//ternary operator
+
+let a = 1 ,b ='1';
+
+let result = (a == b) ? 'equal' : 'Inequal';//triple equal sign is for striked equality.
+console.log(result);
---
+++ source/iterations.js
@@ -0,0 +1,15 @@
+/*
+let marks ;
+
+ for(marks = 1; marks <= 100; marks++){
+console.log('your marks');
+ }
+ */
+
+
+ let i;
+ for(i=1; i<=100; i++){
+    if(i%5 === 0){
+        console.log(i);
+    }
+ }
